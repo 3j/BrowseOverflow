904c8f533f1752fd849d16dd79de8b61dab957d1
David G Chaves <davidgchaves@gmail.com>
2013-06-05 09:29:34 +0200

    Initial Commit

---
+++ BrowseOverflow/DetailViewController.m
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.m
+//  BrowseOverflow
+//
+//  Created by David G Chaves on 6/5/13.
+//  Copyright (c) 2013 David G Chaves. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+- (void)configureView;
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BrowseOverflow/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  BrowseOverflow
+//
+//  Created by David G Chaves on 6/5/13.
+//  Copyright (c) 2013 David G Chaves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
